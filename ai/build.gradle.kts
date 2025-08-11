011b1fc7467d4e4d7eb8a208f5d2163b93a523ab
chime <aguptaq88@gmail.com>
2025-08-11 11:04:06 -0400

    enable splits

--- ai/build.gradle.kts
+++ ai/build.gradle.kts
@@ -31,15 +31,6 @@ android {
         jvmTarget = "11"
     }
 
-    splits {
-        abi {
-            isEnable = true
-            reset()
-            include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
-            isUniversalApk = true
-        }
-    }
-
     sourceSets["main"].jniLibs.srcDirs("src/main/jniLibs")
 
     externalNativeBuild {
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,15 @@ android {
         buildConfig = true
 
     }
+
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+            isUniversalApk = true
+        }
+    }
 }
 
 dependencies {
