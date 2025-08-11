5dd33fa3b5141fd2769dc9acf8bc17953b9c1bdf
chime <aguptaq88@gmail.com>
2025-08-11 11:05:02 -0400

    prepare release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "neth.iecal.questphone"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.6"
+        versionCode = 8
+        versionName = "1.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
     }
