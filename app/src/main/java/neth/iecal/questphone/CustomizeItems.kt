98ba0fc9e2f7219211970709e00123d9360f0a6e
chime <aguptaq88@gmail.com>
2025-09-23 03:20:16 -0400

    add bonsai tree

--- app/src/main/java/neth/iecal/questphone/CustomizeItems.kt
+++ app/src/main/java/neth/iecal/questphone/CustomizeItems.kt
@@ -6,6 +6,7 @@ import neth.iecal.questphone.app.screens.components.NeuralMeshAsymmetrical
 import neth.iecal.questphone.app.screens.components.NeuralMeshSymmetrical
 import neth.iecal.questphone.app.screens.quest.stats.components.HeatMapHomeScreenWrapper
 import neth.iecal.questphone.app.theme.customThemes.BaseTheme
+import neth.iecal.questphone.app.theme.customThemes.BonsaiTheme
 import neth.iecal.questphone.app.theme.customThemes.CherryBlossomsTheme
 import neth.iecal.questphone.app.theme.customThemes.HackerTheme
 import neth.iecal.questphone.app.theme.customThemes.PitchBlackTheme
@@ -13,7 +14,8 @@ import neth.iecal.questphone.app.theme.customThemes.PitchBlackTheme
 val themes: Map<String, BaseTheme> = mapOf(
     "Cherry Blossoms" to CherryBlossomsTheme(),
     "Hacker" to HackerTheme(),
-    "Pitch Black" to PitchBlackTheme()
+    "Pitch Black" to PitchBlackTheme(),
+    "Bonsai" to BonsaiTheme()
 )
 
 const val HOME_WIDGET_PRICE = 30
