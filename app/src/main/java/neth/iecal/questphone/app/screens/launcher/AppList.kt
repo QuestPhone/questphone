80b47d6a26bb7f7c6518b0f375ac5dcc8aa1fba0
chime <aguptaq88@gmail.com>
2025-10-03 07:13:26 -0400

    fix hardlock

--- app/src/main/java/neth/iecal/questphone/app/screens/launcher/AppList.kt
+++ app/src/main/java/neth/iecal/questphone/app/screens/launcher/AppList.kt
@@ -27,6 +27,7 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -97,6 +98,8 @@ fun AppList(navController: NavController, viewModel: AppListViewModel) {
             }
         }
     }
+
+    LaunchedEffect(Unit) { viewModel.loadHardLockedQuests() }
 
     Scaffold { innerPadding ->
         if (showDialog) {
