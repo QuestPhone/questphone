45009dcd7ecad0198d73bbf91a04955d70499cf4
chime <aguptaq88@gmail.com>
2025-10-07 15:19:26 -0400

    comment playservices plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     kotlin("plugin.serialization") version "2.0.20"
     id("com.google.devtools.ksp")
     id("com.google.dagger.hilt.android")
-    id("com.google.gms.google-services")
+//    id("com.google.gms.google-services")
 }
 
 android {
