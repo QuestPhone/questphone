fdd2ae583bb0b7e28f8c4c22f86971d155f077a3
invoke <purwarpranav80@gmail.com>
2025-10-09 11:41:19 +0900

    Bump versionCode and versionName to 23 and 2.6.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "neth.iecal.questphone"
         minSdk = 26
         targetSdk = 36
-        versionCode = 22
-        versionName = "2.6.4"
+        versionCode = 23
+        versionName = "2.6.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
     }
