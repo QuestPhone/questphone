8d736ff81f6e0d2c60bb07daf9cc9d11b071820c
chime <aguptaq88@gmail.com>
2025-10-11 13:26:51 -0400

    change vcode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "neth.iecal.questphone"
         minSdk = 26
         targetSdk = 36
-        versionCode = 23
-        versionName = "2.6.5"
+        versionCode = 24
+        versionName = "2.6.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
     }
